3acb99b586875e6989b579237608b2b20138cb0f
wangdasong <wds_1983@163.com>
2018-05-10 00:13:40 +0800

    1、提交一些idea的配置文件 2、增加scw-auth-server服务的熔断fallback类

---
+++ scw-route-consumer/src/main/java/com/github/wangdasong/scwrouteconsumer/fallback/ScwAuthServerFallbackProvider.java
@@ -0,0 +1,59 @@
+package com.github.wangdasong.scwrouteconsumer.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+@Component
+public class ScwAuthServerFallbackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() {
+        return "scw-auth-server";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return this.getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return this.getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("scw-auth-server不可用".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mt = new MediaType("application", "json", Charset.forName("UTF-8"));
+                headers.setContentType(mt);
+                return headers;
+            }
+        };
+    }
+}
